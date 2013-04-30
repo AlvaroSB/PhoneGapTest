a23db5c49d9d3cb9cebe6eb2a3c7277dadad61d2
Your Name <alvaro@alo.(none)>
2013-04-30 15:35:02 +0200

    TEsting

--- js/main.js
+++ js/main.js
@@ -1,25 +1,11 @@
 var app = {
     
-
-
-// Notification stuff
-
-    showAlert: function (message, title) {
-    if (navigator.notification) {
-        navigator.notification.alert(message, null, title, 'OK');
-    } else {
-        alert(title ? (title + ": " + message) : message);
-    }
-    },
-
-
     initialize: function() {
         var self = this;
         this.store = new MemoryStore(function() {
             $('body').html(new HomeView(self.store).render().el);
         });
     }
-
 
 };
 
