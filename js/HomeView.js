7ef7bd03db0427bc3c346200b2ee706c6462391b
Your Name <alvaro@alo.(none)>
2013-05-21 16:46:02 +0200

    Nanana

--- js/HomeView.js
+++ js/HomeView.js
@@ -1,34 +1,25 @@
 var HomeView = function(store) {
- 
-
-    //WARNING: the findByName function has to be declared first because initialize calls it.
-    
-    this.findByName = function() {
-    store.findByName($('.search-key').val(), function(employees) {
-        $('.employee-list').html(HomeView.liTemplate(employees));
-    });
-    },
 
     this.initialize = function() {
         // Define a div wrapper for the view. The div wrapper is used to attach events.
         this.el = $('<div/>');
         this.el.on('keyup', '.search-key', this.findByName);
-    },
-
-    this.initialize();
-
+    };
 
     this.render = function() {
         this.el.html(HomeView.template());
         return this;
-    }
-  
+    };
 
+    this.findByName = function() {
+        store.findByName($('.search-key').val(), function(employees) {
+            $('.employee-list').html(HomeView.liTemplate(employees));
+        });
+    };
 
-};
+    this.initialize();
 
-    
+}
+
 HomeView.template = Handlebars.compile($("#home-tpl").html());
 HomeView.liTemplate = Handlebars.compile($("#employee-li-tpl").html());
-
-
